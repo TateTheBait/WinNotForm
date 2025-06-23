230958b714edbb2640b30dc167c6cbbfc2af95ef
R3tr0_002 <wrench@solidstatedev.com>
2025-06-23 01:47:05 -0500

    Initial Commit! For VIOLET!!!

--- WindowsNotForm/Form1.Designer.cs
+++ WindowsNotForm/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.AccessibleName = "pictureBox1";
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackColor = SystemColors.Desktop;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 450);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.Dock = DockStyle.Right;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Azure;
+            button1.Location = new Point(680, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 450);
+            button1.TabIndex = 1;
+            button1.Text = "Change HAAR";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        public PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        public OpenFileDialog openFileDialog1;
+        internal Button button1;
     }
 }
--- WindowsNotForm/Form1.cs
+++ WindowsNotForm/Form1.cs
@@ -1,10 +1,76 @@
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
 namespace WindowsNotForm
 {
     public partial class Form1 : Form
     {
+        VideoCapture mainCam = new VideoCapture(0);
+        private CascadeClassifier haar;
+        private bool haarLoaded = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void scan(Mat image)
+        {
+            Image<Gray, Byte> grayImage = image.ToImage<Bgr, byte>().Convert<Gray, Byte>();
+
+
+            var faces = haar.DetectMultiScale(grayImage);
+
+            Random rint = new Random();
+
+            foreach (var face in faces)
+            {
+                CvInvoke.Rectangle(image, face, new MCvScalar(0, 255, 0), 2, Emgu.CV.CvEnum.LineType.EightConnected);
+            }
+        }
+
+
+        private void uploadScreen(object sender, EventArgs e)
+        {
+            Mat frame = new Mat();
+
+            mainCam.Retrieve(frame);
+
+            if (haarLoaded == true)
+            {
+                scan(frame);
+            }
+            
+
+
+            var convertedFrame = frame.ToBitmap();
+            pictureBox1.Invoke(() => pictureBox1.Image = convertedFrame);
+        }
+
+        private void changeHaar(object sender, EventArgs e)
+        {
+            DialogResult selected = openFileDialog1.ShowDialog(this);
+            if (selected == DialogResult.OK)
+            {
+                haarLoaded = true;
+                haar = new CascadeClassifier(openFileDialog1.FileName);
+            } else { haarLoaded = false; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            mainCam.Start();
+
+            openFileDialog1.Filter = "XML files (*.xml)|*.xml";
+            button1.Click += changeHaar;
+
+            mainCam.ImageGrabbed += uploadScreen;
         }
     }
 }
